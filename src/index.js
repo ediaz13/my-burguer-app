c6ef81de8d860d4c844ef19bcbeaa5393c26964e
EMANUEL DIAZ <emadiaz@santandertecnologia.com.ar>
2020-08-03 00:11:07 -0300

    222. Setting Up Routing & Routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
