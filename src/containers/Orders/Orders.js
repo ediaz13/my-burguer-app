898983e00558f5919a8cfc9759047b39cb8175a7
EMANUEL DIAZ <emadiaz@santandertecnologia.com.ar>
2020-08-04 20:24:47 -0300

    230. Fetching Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import Axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
-    render () {
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount() {
+        Axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch(err => {
+                this.setState({loading: false});
+            });
+    }
+
+    render() {
         return (
             <div>
                 <Order />
@@ -13,4 +36,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, Axios) ;
